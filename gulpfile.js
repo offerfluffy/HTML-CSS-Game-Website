11bfed26a6b25ef189433b0fd7d5072ad5509591
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-05-11 13:46:59 +0200

    Project Build

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+// compile only your main entry-point
+function buildStyles() {
+  return src("./scss/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./css/"));
+}
+
+// watch but then call done()
+function watchTask(done) {
+  watch(["./scss/**/*.scss"], buildStyles);
+  done();
+}
+
+exports.default = series(buildStyles, watchTask);
